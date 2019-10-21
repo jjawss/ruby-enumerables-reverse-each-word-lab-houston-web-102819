3a95fdfda44ceef01840e55a932a9ba253cde879
David Jaworski <jaws360@gmail.com>
2019-10-21 23:03:34 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,6 +1,9 @@
 def reverse_each_word(string)
-  array = string
-  p array[0]
+  counter = 0
+  while string[counter]
+    if string[counter = " "]
+      
+  end
 end
 
 p reverse_each_word("Hello there, and how are you?")
