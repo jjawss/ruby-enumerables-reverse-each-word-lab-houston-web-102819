b267ddd199a728182cc8ffe8b7990426fb694686
David Jaworski <jaws360@gmail.com>
2019-10-21 23:21:24 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,5 +2,7 @@ def reverse_each_word(string)
   return string.split
 end
 
-p reverse_each_word("Hello there, and how are you?")
+reverse_each_word("Hello there, and how are you?") do
+    
+end
   
