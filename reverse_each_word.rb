fe64acf698d889627608f166ce3682804a1ae520
David Jaworski <jaws360@gmail.com>
2019-10-21 23:48:23 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,11 +1,13 @@
 def reverse_each_word(string)
   return string.split
+  puts string
 end
 #=> ["Hello", "there", "and", "how", "are", "you?"]
-reverse_each_word("Hello there, and how are you?").map do |string|
+
+reverse_each_word("Hello there, and how are you?").map do |word|
   counter = 0 
-  while string[counter]
-    string[counter] = string[counter*-1]
+  while word[counter]
+    word[counter] = word[counter*-1]
     counter+=1
   end
 end
