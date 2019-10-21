2f4444d84fa663c221f5ba50213c4bebbe7fc8fd
David Jaworski <jaws360@gmail.com>
2019-10-21 23:55:56 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -7,7 +7,7 @@ reverse_each_word("Hello there, and how are you?").map do |word|
   counter = 0
   last_counter = -1
   while word[counter]
-    word[counter] = word[counter*-1]
+    word[counter] = word[last_counter]
     counter+=1
     last_counter -=1
   end
