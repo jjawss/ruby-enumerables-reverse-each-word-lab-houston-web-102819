20231a71e2035149c55350fd198dca1b0880852c
David Jaworski <jaws360@gmail.com>
2019-10-22 00:21:37 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -3,9 +3,10 @@ def reverse_each_word(string)
 end
 #=> ["Hello", "there", "and", "how", "are", "you?"]
 
-p reverse_each_word("Hello there, and how are you?").each do |word|
+reverse_each_word("Hello there, and how are you?").each do |word|
   counter = word.length-2  
   word.reverse
+  p word
   end
 
 # << to append to a string
