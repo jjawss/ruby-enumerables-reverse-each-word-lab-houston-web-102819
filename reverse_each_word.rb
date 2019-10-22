f577f0b17956a3f2a9950a424c362e2ef9185aea
David Jaworski <jaws360@gmail.com>
2019-10-22 00:04:10 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -3,5 +3,5 @@ def reverse_each_word(string)
 end
 #=> ["Hello", "there", "and", "how", "are", "you?"]
 
-reverse_each_word("Hello there, and how are you?").reverse_each {|word| }
+p reverse_each_word("Hello there, and how are you?").reverse_each {|word| }
   
