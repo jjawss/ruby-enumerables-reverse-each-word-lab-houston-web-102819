d73beede99c78dd0afeff0ebf50092a016011411
David Jaworski <jaws360@gmail.com>
2019-10-22 00:30:30 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,10 +1,6 @@
 def reverse_each_word(string)
   string.split
-end
-#=> ["Hello", "there", "and", "how", "are", "you?"]
-
-reverse_each_word("Hello there, and how are you?").collect do |word|
-  word.reverse
-end
-
-# << to append to a string
+  string.collect do |word|
+    word.reverse
+  end
+end  
